2fc2cda2f56c27770702a05861b3aa4798de32b7
shikha-max <tshikha66@mail.com>
2021-12-15 13:51:35 +0530

    login

---
+++ src/components/Contextprovider.jsx
@@ -0,0 +1,14 @@
+import { createContext } from "react";
+import { useState } from "react";
+export const AutheContext= createContext()
+export const AutheContextprovider=({children})=>{
+    const [token,setToken] = useState("")
+    const change=(value)=>{
+        setToken(value)
+    }
+    return <>
+
+    <AutheContext.Provider value={{state:token,f:change}}>{children}</AutheContext.Provider>
+
+    </>
+}
--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -8,47 +8,48 @@ export const Dashboard=()=>{
     useEffect(()=>{
      let hash= getTokenFromResponse()
      setToken(hash.access_token)
+     hash="";
      spotifyApi.setAccessToken(token)
      spotifyApi.getMe().then((user) => {
         console.log(user)
       });
 
-      spotifyApi.searchTracks('friends')
-  .then((data)=> {
-    console.log('Search by "Love"', data);
-  })
+//       spotifyApi.searchTracks('friends')
+//   .then((data)=> {
+//     console.log('Search by "Love"', data);
+//   })
 
   
   
-  spotifyApi.getCategories({
-    limit : 5,
-    offset: 0,
-    country: 'SE',
-    locale: 'sv_SE'
-})
-.then(function(data) {
-  console.log(data);
-}, function(err) {
-  console.log("Something went wrong!", err);
-});
+//   spotifyApi.getCategories({
+//     limit : 5,
+//     offset: 0,
+//     country: 'SE',
+//     locale: 'sv_SE'
+// })
+// .then(function(data) {
+//   console.log(data);
+// }, function(err) {
+//   console.log("Something went wrong!", err);
+// });
 
 
   
-  spotifyApi.getArtistAlbums('2ryKHw6BaxKXC1KhRp4Nh1')
-  .then(function(data) {
-    console.log('Artist albums', data);
-  }, function(err) {
-    console.error(err);
-  });
-  spotifyApi.getAudioFeaturesForTrack("08bNPGLD8AhKpnnERrAc6G")
-  .then(function(data) {
-    console.log(data);
-  })
+  // spotifyApi.getArtistAlbums('2ryKHw6BaxKXC1KhRp4Nh1')
+  // .then(function(data) {
+  //   console.log('Artist albums', data);
+  // }, function(err) {
+  //   console.error(err);
+  // });
+  // spotifyApi.getAudioFeaturesForTrack("08bNPGLD8AhKpnnERrAc6G")
+  // .then(function(data) {
+  //   console.log(data);
+  // })
     
-  spotifyApi.getAudioAnalysisForTrack('08bNPGLD8AhKpnnERrAc6G')
-  .then(function(data) {
-    console.log(data);
-  });
+  // spotifyApi.getAudioAnalysisForTrack('08bNPGLD8AhKpnnERrAc6G')
+  // .then(function(data) {
+  //   console.log(data);
+  // });
 
     },[token])
     
